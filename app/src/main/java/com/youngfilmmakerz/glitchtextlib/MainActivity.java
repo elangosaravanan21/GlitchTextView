77eb761e2afd5f9d3a5318e4df38c5eff505e22f
elango <elangovan@thinksynq.in>
2019-09-23 18:03:41 +0530

    desgin updated

--- app/src/main/java/com/youngfilmmakerz/glitchtextlib/MainActivity.java
+++ app/src/main/java/com/youngfilmmakerz/glitchtextlib/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.youngfilmmakerz.glitchtext.Glitchtext;
 
+
 public class MainActivity extends AppCompatActivity {
     Glitchtext glitchtext;
 
@@ -13,8 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        glitchtext=(Glitchtext)findViewById(R.id.glitchtext);
-        glitchtext.setMaintext("Hello World");
+        glitchtext = (Glitchtext) findViewById(R.id.glitchtext);
+        glitchtext.setMaintext("YOUR \nNAME");
         glitchtext.setFontstyle("Bugfast.otf");
+        glitchtext.setTextsize(35);
     }
 }
